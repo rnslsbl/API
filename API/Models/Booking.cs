21c0d8936e83cd1a846d70cded27e8610fbabd65
rnslsbl <ronasalsabila1391@gmail.com>
2023-05-17 10:27:03 +0700

    Initial commit

---
+++ API/Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models; 
+    public class Booking
+    {
+    public Guid Guid { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Status { get; set; }
+    public string Remarks { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedOne { get; set; }
+    public Guid RoomGuid { get; set; }
+    public Guid EmployeeGuid { get; set; }
+}
+
